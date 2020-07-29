4c1930f627808b9b5c7d89faa3461057b14883d0
kyme254 <kimanzitech254@gmail.com>
2020-07-29 11:42:22 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+
+
+define('YII_ENABLE_ERROR_HANDLER', false);
+define('YII_ENABLE_EXCEPTION_HANDLER', false);
+ini_set("display_errors",false);
+//error_reporting(E_ERROR | E_WARNING | E_PARSE);
+
+// include Yii bootstrap file
+require_once(dirname(__FILE__).'/yiiframework/yii.php');
+$config=dirname(__FILE__).'/protected/config/main.php';
+
+// create a Web application instance and run
+Yii::createWebApplication($config)->run();
